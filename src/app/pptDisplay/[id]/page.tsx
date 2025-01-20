0758525dafc4251dc1fa4f882a2e6398de6b4a60
Siddharth Choudhary <csiddharth380@gmail.com>
2025-01-20 08:11:57 +0530

    Changed alot

--- src/app/pptDisplay/[id]/page.tsx
+++ src/app/pptDisplay/[id]/page.tsx
@@ -40,17 +40,36 @@ export default function Page() {
 
         const { data: url1, error: err1 } = await supabase
           .from("company")
-          .select(data.name)
-          .select("url1");
+          .select("url1")
+          .eq("ticker", id)
+          .single();
         const { data: url2, error: err2 } = await supabase
           .from("company")
-          .select(data.name)
-          .select("url2");
+          .select("url2")
+          .eq("ticker", id)
+          .single();
 
-        if (!url1) data.url1 = await getImage(data.name);
-        else data.url1 = url1.url1;
-        if (!url2) data.url2 = await getImage(data.name);
-        else data.url2 = url2.url2;
+        console.log(url1, url2);
+
+        if (!url1) {
+          data.url1 = await getEncodedImage(await getImage(data.name));
+
+          const { data: d, error } = await supabase
+            .from("company")
+            .update({ url1: data.url1 })
+            .eq("name", data.name);
+          if (error) console.log("Image upload error", error);
+        } else data.url1 = url1.url1;
+
+        if (!url2) {
+          data.url2 = await getEncodedImage(await getImage(data.name));
+
+          const { data: d, error } = await supabase
+            .from("company")
+            .update({ url2: data.url2 })
+            .eq("name", data.name);
+          if (error) console.log("Image upload error", error);
+        } else data.url2 = url2.url2;
 
         setStockData(data);
       } catch (err) {
@@ -69,18 +88,35 @@ export default function Page() {
   if (error) return <div className="text-red-500">{error}</div>;
   if (!stockData) return <div>No data available</div>;
 
+  console.log(stockData);
+
   return <StockDataDisplay userId={userId} id={id} data={stockData} />;
 }
 
 async function getImage(_name: string) {
-  const data = { stockName: _name };
-  const res = await fetch("/api/image", {
+  // const data = { stockName: _name };
+  // const res = await fetch("/api/image", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data),
+  // });
+  // const response = await res.json();
+  // return response.imageUrl;
+  return "https://plus.unsplash.com/premium_photo-1698405316329-fd9c43d7e11c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fG5vcndheXxlbnwwfHwwfHx8MA%3D%3D";
+}
+
+async function getEncodedImage(imageUrl: string) {
+  const data = { imageUrl: imageUrl };
+  const res = await fetch("/api/extractImage", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
-  const response = await res.json();
-  return response.imageUrl;
+  const image = (await res.json()).base64Image;
+
+  return image;
 }
