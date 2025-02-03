aa20097e8739c28360b498f70e58bde44fe1e72d
Siddharth Choudhary <csiddharth380@gmail.com>
2025-02-03 13:47:19 +0530

    Minor

--- src/app/wipe/page.tsx
+++ src/app/wipe/page.tsx
@@ -1,80 +1,64 @@
 "use client";
 
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { createSupabaseClient } from "@/lib/supaBaseClient";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 export default function Home() {
-  const interBubbleRef = useRef<HTMLDivElement>(null);
-  const [topic, setTopic] = useState<string>("");
-  const [typed, setTyped] = useState<boolean>(false);
-  const [ticker, setTicker] = useState<string>("");
-  const [userLogged, setUserLogged] = useState(false);
+  const [stockName, setStockName] = useState<string>("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const router = useRouter();
 
-  useEffect(() => {
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
+  const handleStockNameInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setStockName(e.target.value);
+  };
 
-    const handleMouseMove = (event: MouseEvent) => {
-      const interBubble = interBubbleRef.current;
-      if (!interBubble) return;
+  const handlePasswordInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
-      const rect = interBubble.getBoundingClientRect();
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    setMessage(""); // Reset the message
 
-      tgX = event.clientX - rect.width / 2;
-      tgY = event.clientY - rect.height / 2;
-    };
+    const adminPassword = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
+    if (password !== adminPassword) {
+      setMessage("Invalid password. Please try again.");
+      setPassword("");
+      setIsLoading(false);
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
 
-    const move = () => {
-      curX += (tgX - curX) / 5;
-      curY += (tgY - curY) / 5;
-      if (interBubbleRef.current) {
-        interBubbleRef.current.style.transform = `translate3d(${Math.round(
-          curX
-        )}px, ${Math.round(curY)}px, 0)`;
-      }
-      requestAnimationFrame(move);
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-    move();
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
-  const handleInput = async (e: ChangeEvent<HTMLInputElement>) => {
-    const val = e.target.value;
-    setTopic(val);
-
-    if (val.length === 0) {
-      setTicker("");
       return;
     }
 
+    const supabase = createSupabaseClient();
     try {
-      const response = await fetch("/api/ticker", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ stockName: val }),
-      });
+      const { data, error } = await supabase
+        .from("company")
+        .delete()
+        .eq("ticker", stockName);
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      if (error) {
+        console.error("Delete error:", error.message);
+        setMessage("An error occurred. Please try again.");
+      } else {
+        setMessage("Stock data deleted successfully.");
+        setTimeout(() => {
+          setMessage("");
+        }, 3000);
       }
-
-      const data = await response.json();
-      setTicker(data.ticker);
-    } catch (err) {
-      console.error("Error fetching ticker:", err);
-      setTicker("");
+    } catch (error: any) {
+      console.error("Caught error:", error);
+      setMessage("An error occurred. Please try again.");
     } finally {
-      setTyped(true);
+      setIsLoading(false);
     }
   };
 
@@ -94,53 +78,62 @@ export default function Home() {
               <br />
               with a Single Click
             </h2>
-            <input
-              type="text"
-              onChange={handleInput}
-              value={topic}
-              className="flex-grow p-3 text-black rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-pink-400"
-              placeholder="Enter stock topic"
-            />
-            <input
-              type="text"
-              onChange={handleInput}
-              value={topic}
-              className="flex-grow p-3 text-black rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-pink-400"
-              placeholder="Enter stock topic"
-            />
-            <input
-              type="text"
-              onChange={handleInput}
-              value={topic}
-              className="flex-grow p-3 text-black rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-pink-400"
-              placeholder="Enter stock topic"
-            />
           </div>
 
-          <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 w-full max-w-md">
+          <div className="flex flex-col space-y-4 w-full max-w-md">
             <input
               type="text"
-              onChange={handleInput}
-              value={topic}
-              className="flex-grow p-3 text-black rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-pink-400"
-              placeholder="Enter stock topic"
+              onChange={handleStockNameInput}
+              value={stockName}
+              className="p-3 text-black rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-pink-400"
+              placeholder="Enter Ticker name"
             />
+            <div className="relative flex items-center">
+              <input
+                type={showPassword ? "text" : "password"} // Toggle between text and password types
+                onChange={handlePasswordInput}
+                value={password}
+                className="p-3 text-black rounded-lg shadow-md w-full focus:outline-none focus:ring-2 focus:ring-pink-400"
+                placeholder="Enter password"
+              />
+              <Button
+                type="button"
+                variant="ghost"
+                size="icon"
+                className="absolute right-1 top-1/2 transform -translate-y-1/2 h-4 w-8 px-1 py-5 hover:bg-black hover:bg-opacity-10"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <EyeOffIcon className="h-4 w-4 text-gray-300" />
+                ) : (
+                  <EyeIcon className="h-4 w-4 text-gray-300" />
+                )}
+                <span className="sr-only">
+                  {showPassword ? "Hide password" : "Show password"}
+                </span>
+              </Button>
+            </div>
             <button
+              onClick={handleSubmit}
+              disabled={isLoading || !stockName || !password}
               className={`px-6 py-3 ${
-                typed
-                  ? "bg-gradient-to-r from-pink-500 to-purple-600"
-                  : "bg-slate-600"
-              } rounded-lg text-white font-semibold shadow-md hover:from-pink-600 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 transition-all duration-300`}
-              disabled={!typed}
+                isLoading || !stockName || !password
+                  ? "bg-slate-600"
+                  : "bg-gradient-to-r from-pink-500 to-purple-600 hover:from-pink-600 hover:to-purple-700"
+              } rounded-lg text-white font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 transition-all duration-300`}
             >
-              Generate PPT
+              {isLoading ? "Processing..." : "Delete Stock Data"}
             </button>
           </div>
+
+          {message && (
+            <p className="mt-4 text-center text-lg font-semibold">{message}</p>
+          )}
         </main>
 
         <footer className="w-full bg-black bg-opacity-20 backdrop-blur-sm py-4">
           <div className="container mx-auto px-4 text-center text-sm">
-            © 2025 pptX. All rights reserved.
+            © 2025 StockGen. All rights reserved.
           </div>
         </footer>
       </div>
@@ -150,7 +143,6 @@ export default function Home() {
       <div className="g3"></div>
       <div className="g4"></div>
       <div className="g5"></div>
-      <div id="bubble" ref={interBubbleRef} className="interactive"></div>
     </div>
   );
 }
