f308ed628d66d3c0cd48005db86ea4556b185106
Siddharth Choudhary <csiddharth380@gmail.com>
2025-02-20 20:57:01 +0530

    changes in editable component

--- src/components/editableTest.tsx
+++ src/components/editableTest.tsx
@@ -1,3 +1,5 @@
+import { createSupabaseClient } from "@/lib/supaBaseClient";
+import { X } from "lucide-react";
 import type React from "react";
 import {
   useState,
@@ -8,26 +10,51 @@ import {
 } from "react";
 
 interface EditableTextProps {
+  id?: string;
   initialText: string;
-  onSave: (text: string) => void;
+  onSave: (text: string, image?: File) => void;
   className?: string;
   mitigation?: boolean;
   hasChanged: boolean;
+  count?: number;
+  setCount?: Dispatch<SetStateAction<number>>;
   setHasChanged: Dispatch<SetStateAction<boolean>>;
 }
 
 export function EditableText({
+  id,
   initialText,
   hasChanged,
   setHasChanged,
   mitigation = false,
+  count,
+  setCount,
   onSave,
   className = "",
 }: EditableTextProps) {
   const [isEditing, setIsEditing] = useState(false);
   const [text, setText] = useState(initialText);
+  const [pastedImage, setPastedImage] = useState<File | null>(null);
   const textRef = useRef<HTMLDivElement>(null);
   const cursorPositionRef = useRef<number | null>(null);
+  const [dbImage, setDbImage] = useState("");
+  const [hasImage, seHasImage] = useState(false);
+  const client = createSupabaseClient();
+
+  useEffect(() => {
+    const func = async () => {
+      const { data, error } = await client
+        .from("images")
+        .select("img")
+        .eq("ticker", id)
+        .single();
+      if (data) {
+        setDbImage(data.img);
+        seHasImage(true);
+      }
+    };
+    func();
+  }, []);
 
   useEffect(() => {
     if (isEditing && textRef.current) {
@@ -77,8 +104,73 @@ export function EditableText({
     }
   };
 
+  const handlePaste = async (e: React.ClipboardEvent<HTMLDivElement>) => {
+    const items = e.clipboardData.items;
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].type.indexOf("image") !== -1) {
+        e.preventDefault();
+        const file = items[i].getAsFile();
+        if (file) {
+          setPastedImage(file);
+          setHasChanged(true);
+          await uploadImage(file);
+        }
+        break;
+      }
+    }
+    seHasImage(false);
+  };
+
+  const uploadImage = async (file: File) => {
+    const encodedImage = await getEncodedImage(file);
+    const { data, error } = await client
+      .from("images")
+      .upsert([{ ticker: id, img: encodedImage }])
+      .single();
+    console.log(data, error);
+  };
+
+  async function getEncodedImage(imageFile: File) {
+    const base64Image = await fileToBase64(imageFile);
+
+    const data = { imageUrl: base64Image };
+    const res = await fetch("/api/extractImage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const image = (await res.json()).base64Image;
+    return image;
+  }
+
+  function fileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = (error) => reject(error);
+    });
+  }
+
   const handleSave = () => {
-    onSave(text);
+    onSave(text, pastedImage || undefined);
+  };
+
+  const handleCut = async () => {
+    const { data, error } = await client
+      .from("images")
+      .delete()
+      .eq("ticker", id)
+      .single();
+    console.log(data, error);
+
+    if (pastedImage) setPastedImage(null);
+    else if (hasImage) seHasImage(false);
   };
 
   return (
@@ -92,6 +184,7 @@ export function EditableText({
         onBlur={handleSave}
         onClick={() => setIsEditing(true)}
         onInput={handleInput}
+        onPaste={handlePaste}
         suppressContentEditableWarning={true}
       >
         {mitigation && (
@@ -99,6 +192,36 @@ export function EditableText({
         )}
         {text}
       </div>
+      {hasImage && (
+        <div className="mt-2">
+          <button
+            className="absolute right-0 bg-slate-900 rounded-full p-1 bg-opacity-50 m-1"
+            onClick={handleCut}
+          >
+            <X />
+          </button>
+          <img
+            src={dbImage || "/placeholder.svg"}
+            alt="Database image"
+            className="w-full max-w-[681px] h-auto aspect-[681/416] object-cover mx-auto rounded-lg shadow-lg"
+          />
+        </div>
+      )}
+      {pastedImage && (
+        <div className="mt-2">
+          <button
+            className="absolute right-0 bg-slate-900 rounded-full p-1 bg-opacity-50 m-1"
+            onClick={handleCut}
+          >
+            <X />
+          </button>
+          <img
+            src={URL.createObjectURL(pastedImage) || "/placeholder.svg"}
+            alt="Pasted image"
+            className="w-full max-w-[681px] h-auto aspect-[681/416] object-cover mx-auto rounded-lg shadow-lg"
+          />
+        </div>
+      )}
     </div>
   );
 }
